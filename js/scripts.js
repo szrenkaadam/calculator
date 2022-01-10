f7e2d6f194a582cc5cfcc9bc96a409ce01761d86
szrenkaadam <78079460+szrenkaadam@users.noreply.github.com>
2022-01-10 13:12:22 +0100

    input értékek összegyűjtése

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function szamol() {
+ let szam1Szoveg = document.getElementById("szam1").value
+ let szam2Szoveg = document.getElementById("szam2").value
+
+ console.log(`Első szám: ${szam1Szoveg}`)
+ console.log(`Második szám: ${szam2Szoveg}`)
+}
+
+document.getElementById("szam1").focus()
